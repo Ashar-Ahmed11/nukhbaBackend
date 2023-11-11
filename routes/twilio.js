fa2b8977b17aa2f5b7476ed9c8fe36fbf1e99ecb
karma-2day <ashar.ahmed7307@hhs.edu.pk>
2023-11-12 01:23:18 +0500

    hellokarma

--- routes/twilio.js
+++ routes/twilio.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router()
 const accountSid = 'AC618f765df3b485f886848c1f0e5f063b';
 const authToken = '9be5bbc6d4ed25e104f9b1fb482c2106';
-const { Rembg } = require("rembg-node");
-const sharp = require("sharp")
-const got = require("got");
-const fs = require('fs')
+// const { Rembg } = require("rembg-node");
+// const sharp = require("sharp")
+// const got = require("got");
+// const fs = require('fs')
 
 
 const client = require('twilio')(accountSid, authToken);
@@ -30,72 +30,72 @@ router.post('/', async (req, res) => {
     res.send(req.body)
 })
 
-router.post('/rembg', async (req, res) => {
-    try {
+// router.post('/rembg', async (req, res) => {
+//     try {
 
 
-        const imageBuffer = await got(req.body.url).buffer();
-        const input = sharp(imageBuffer);
+//         const imageBuffer = await got(req.body.url).buffer();
+//         const input = sharp(imageBuffer);
 
-        // optional arguments
-        const rembg = new Rembg({
-            logging: true,
-        });
+//         // optional arguments
+//         const rembg = new Rembg({
+//             logging: true,
+//         });
 
-        const output = await rembg.remove(input);
+//         const output = await rembg.remove(input);
 
 
-        await output.png().toFile("helloworl2.png");
+//         await output.png().toFile("helloworl2.png");
         
-        // const img= fetch('')
+//         // const img= fetch('')
 
 
 
-        function toBase64(filePath) {
-            const img = fs.readFileSync(filePath);
+//         function toBase64(filePath) {
+//             const img = fs.readFileSync(filePath);
           
-            return Buffer.from(img).toString('base64');
-          }
+//             return Buffer.from(img).toString('base64');
+//           }
 
 
-          const myimage = toBase64('helloworl2.png')
-          const withPrefix = 'data:image/png;base64,' + myimage
+//           const myimage = toBase64('helloworl2.png')
+//           const withPrefix = 'data:image/png;base64,' + myimage
 
-        const url = "https://api.cloudinary.com/v1_1/dextrzp2q/image/upload"
+//         const url = "https://api.cloudinary.com/v1_1/dextrzp2q/image/upload"
 
-        // https://faithful-bass-yoke.cyclic.app/api/sendImg/
+//         // https://faithful-bass-yoke.cyclic.app/api/sendImg/
     
-        const formData = new FormData()
+//         const formData = new FormData()
     
-        formData.append('file', withPrefix)
-        formData.append('upload_preset', 'dga8po59')
+//         formData.append('file', withPrefix)
+//         formData.append('upload_preset', 'dga8po59')
     
-        const response = await fetch(url, {
-          method: 'post', // *GET, POST, PUT, DELETE, etc.
+//         const response = await fetch(url, {
+//           method: 'post', // *GET, POST, PUT, DELETE, etc.
     
-          headers: {
-            "ngrok-skip-browser-warning": "69420",
-          },
+//           headers: {
+//             "ngrok-skip-browser-warning": "69420",
+//           },
     
-          body: formData // body data type must match "Content-Type" header
-        });
-        const myurl = await response.json()
-    //     // const contents = await fs.readFile('helloworl.webp',{encoding:'base64url'});
+//           body: formData // body data type must match "Content-Type" header
+//         });
+//         const myurl = await response.json()
+//     //     // const contents = await fs.readFile('helloworl.webp',{encoding:'base64url'});
        
 
-    //    const imgResult = await cloudinary.uploader.upload(changeimg)
+//     //    const imgResult = await cloudinary.uploader.upload(changeimg)
        
-    //     // // optionally you can use .trim() too!
-    //     // // await output.trim().webp().toFile("test-output-trimmed.webp");
+//     //     // // optionally you can use .trim() too!
+//     //     // // await output.trim().webp().toFile("test-output-trimmed.webp");
 
 
 
-        res.send(myurl)
-    } catch (error) {
-        res.status(500).send("Some iNTERNAL sERVER ERROR")
-        console.error(error)
-    }
-})
+//         res.send(myurl)
+//     } catch (error) {
+//         res.status(500).send("Some iNTERNAL sERVER ERROR")
+//         console.error(error)
+//     }
+// })
 
 
 
