6ae061686703acde0b2c0ab5c58f9065ef9a768d
Ashar <ashar.a.hmd11@gmail.com>
2024-08-12 00:35:50 -0700

    new version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = process.env.PORT || 8000
 const cors = require('cors')
 const connectToMongo = require('./db')
 connectToMongo()
